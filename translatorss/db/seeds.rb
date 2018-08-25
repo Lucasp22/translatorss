0610761ad8320f16104afcf1a34af3e4bdfeeb1d
Lucas Padua <lucas.p2222@gmail.ciom>
2018-08-25 20:06:26 +1000

    Login

--- translatorss/db/seeds.rb
+++ translatorss/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Services.destroy_all
+
+s1 = Service.create :title => '1231'
+s2 = Service.create :title => '1232'
+s3 = Service.create :title => '1233'
+
+User.destroy_all
+u1 = User.create :email => 'craigsy@ga.co', :password => 'chicken'
+u2 = User.create :email => 'jonesy@ga.co', :password => 'chicken'
+
+u1.services << s2
+u2.services << s1
+u2.services << s3
